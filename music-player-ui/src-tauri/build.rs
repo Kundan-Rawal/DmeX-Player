802138cec8062c9764b75e4ac436666265ac27e0
Kundan-Rawal <rawalkundan987@gmail.com>
2026-03-21 02:48:56 +0530

    Refactored the build.rs 2

--- music-player-ui/src-tauri/build.rs
+++ music-player-ui/src-tauri/build.rs
@@ -1,5 +1,8 @@
 fn main() {
-    // FIX: Added an extra ../ to go up two folder levels to the Root directory
+    // FIX: Restore the core Tauri build script! (This generates the missing Gradle files)
+    tauri_build::build();
+
+    // Compile the custom C++ DSP engine
     println!("cargo:rerun-if-changed=../../audio-engine-cpp/main.cpp");
     
     cc::Build::new()
