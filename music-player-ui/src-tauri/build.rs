f0c5ae063ffb372f5bb2a05a789f2ab535e50c1d
Kundan-Rawal <rawalkundan987@gmail.com>
2026-03-21 02:39:17 +0530

    Refactored the build.rs

--- music-player-ui/src-tauri/build.rs
+++ music-player-ui/src-tauri/build.rs
@@ -1,8 +1,9 @@
 fn main() {
-    println!("cargo:rerun-if-changed=../audio-engine-cpp/main.cpp");
+    // FIX: Added an extra ../ to go up two folder levels to the Root directory
+    println!("cargo:rerun-if-changed=../../audio-engine-cpp/main.cpp");
     
     cc::Build::new()
         .cpp(true)
-        .file("../audio-engine-cpp/main.cpp")
+        .file("../../audio-engine-cpp/main.cpp")
         .compile("audioengine");
 }
